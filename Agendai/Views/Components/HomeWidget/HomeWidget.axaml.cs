9721a1b0c272c16a597b77ffff47eb44df49cd3e
trsaints <thiago.dev2k3@gmail.com>
2025-03-23 20:00:26 -0300

    feat: finish base structure for Home context

---
+++ Agendai/Views/Components/HomeWidget/HomeWidget.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+
+namespace Agendai.Views.Components.HomeWidget;
+
+
+public partial class HomeWidget : UserControl
+{
+	public HomeWidget() { InitializeComponent(); }
+}
+
