1f8a3d3f81a4d4e4d4eca19e1fe2156f749459bc
trsaints <thiago.dev2k3@gmail.com>
2025-03-23 19:37:43 -0300

    feat: create custom TodoControl control for listing recent tasks and to create new ones

---
+++ Agendai/Views/Components/TodoControl/TodoControl.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+
+namespace Agendai.Views.Components.TodoControl;
+
+
+public partial class TodoControl : UserControl
+{
+	public TodoControl() { InitializeComponent(); }
+}
+
