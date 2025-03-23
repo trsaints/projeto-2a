92909dcfc88d6ef39046b368e32185458f3e0eb5
trsaints <thiago.dev2k3@gmail.com>
2025-03-23 19:25:59 -0300

    feat: create custom Timer control

---
+++ Agendai/Views/Components/Timer/Timer.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+
+namespace Agendai.Views.Components.Timer;
+
+
+public partial class Timer : UserControl
+{
+	public Timer() { InitializeComponent(); }
+}
+
