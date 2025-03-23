76a858e478b35a90f4648e96f734a6cc5e247b4d
trsaints <thiago.dev2k3@gmail.com>
2025-03-23 19:09:30 -0300

    feat: create SideBar control

---
+++ Agendai/Views/Components/SideBar.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+
+namespace Agendai.Views.Components;
+
+
+public partial class SideBar : UserControl
+{
+	public SideBar() { InitializeComponent(); }
+}
+
---
+++ Agendai/Views/Components/TodoList.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+
+namespace Agendai.Views.Components;
+
+
+public partial class TodoList : UserControl
+{
+	public TodoList() { InitializeComponent(); }
+}
+
