b197f11bc3f7f22b7907250af38cc834ea630d2f
trsaints <thiago.dev2k3@gmail.com>
2025-03-30 08:30:50 -0300

    feat: render events based on actual (mockup) data

---
+++ Agendai/ViewModels/EventListViewModel.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.ObjectModel;
+using Agendai.Models;
+
+
+namespace Agendai.ViewModels;
+
+
+public class EventListViewModel : ViewModelBase
+{
+	public EventListViewModel()
+	{
+		Events =
+		[
+			new Event(1, "Conferência da Pamonha")
+			{
+				Description = "Conferência de pamonhas",
+				Due = new DateTime(2021, 10, 10, 8, 0, 0),
+				Repeats = Repeats.Anually
+			},
+			new Event(2, "Feira da Foda")
+			{
+				Description = "Coferência do Agro",
+				Due = new DateTime(2025, 12, 5),
+				Repeats = Repeats.None
+			},
+			new Event(3, "Festa do Peão")
+			{
+				Description = "Festa do Peão de Barretos",
+				Due = new DateTime(2021, 8, 20),
+				Repeats = Repeats.Monthly
+			}
+		];
+	}
+	
+	public ObservableCollection<Event> Events { get; set; }
+}
---
+++ Agendai/Views/Components/EventList/EventList.axaml.cs
@@ -0,0 +1,17 @@
+using Agendai.ViewModels;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+
+namespace Agendai.Views.Components.EventList;
+
+
+public partial class EventList : UserControl
+{
+	public EventList()
+	{
+		InitializeComponent();
+		DataContext = new EventListViewModel();
+	}
+}
