2a9f78d1ce4b6671c60c7425e098645fec76f57c
trsaints <thiago.dev2k3@gmail.com>
2025-03-30 12:10:13 -0300

    update: adjust view locator for context switching based on app's naming convention

--- Agendai/ViewLocator.cs
+++ Agendai/ViewLocator.cs
@@ -1,31 +1,37 @@
 using System;
+using Agendai.ViewModels;
 using Avalonia.Controls;
 using Avalonia.Controls.Templates;
-using Agendai.ViewModels;
+
 
 namespace Agendai;
 
+
 public class ViewLocator : IDataTemplate
 {
+	public Control Build(object param)
+	{
+		var viewModelType = param.GetType();
+		
+		// assure the string matches the naming convention, for avoiding
+		// inconsistencies such as "HomeWindowWindow" in the context search
+		// after replacing "ViewModel" with "Window"
+		
+		var windowName = viewModelType.Name.Replace("ViewModel", "Window")
+		                              .Replace("WindowWindow", "Window"); 
+		
+		// windowName is doubled because each window is within its own namespace
+		// example: Agendai.Views.Windows.HomeWindow.HomeWindow
+		var fullNamespace = $"Agendai.Views.Windows.{windowName}.{windowName}";
+		var viewType      = Type.GetType(fullNamespace);
 
-    public Control? Build(object? param)
-    {
-        if (param is null)
-            return null;
-        
-        var name = param.GetType().FullName!.Replace("ViewModel", "View", StringComparison.Ordinal);
-        var type = Type.GetType(name);
+		if (viewType is not null)
+		{
+			return (Control)Activator.CreateInstance(viewType)!;
+		}
 
-        if (type != null)
-        {
-            return (Control)Activator.CreateInstance(type)!;
-        }
-        
-        return new TextBlock { Text = "Not Found: " + name };
-    }
+		return new TextBlock { Text = $"View not found for {fullNamespace}" };
+	}
 
-    public bool Match(object? data)
-    {
-        return data is ViewModelBase;
-    }
+	public bool Match(object param) { return param is ViewModelBase; }
 }
