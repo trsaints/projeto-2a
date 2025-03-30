1489d69736fa2a24db930978307c4486d7cfb4ac
trsaints <thiago.dev2k3@gmail.com>
2025-03-30 12:09:01 -0300

    feat: create new window for context switching

---
+++ Agendai/ViewModels/AgendaWindowViewModel.cs
@@ -0,0 +1,7 @@
+namespace Agendai.ViewModels;
+
+
+public class AgendaWindowViewModel : ViewModelBase
+{
+	public string Title { get; set; } = "Agenda";
+}
---
+++ Agendai/Views/Windows/AgendaWindow/AgendaWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+
+namespace Agendai.Views.Windows.AgendaWindow;
+
+
+public partial class AgendaWindow : UserControl
+{
+	public AgendaWindow() { InitializeComponent(); }
+}
+
