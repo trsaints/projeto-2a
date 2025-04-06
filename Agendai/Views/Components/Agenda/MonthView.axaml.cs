cc11e9da0f5681126eae78355454dd1e1cd03b24
Xustavo <gugaxavier2005@gmail.com>
2025-04-06 11:44:04 -0300

    refactor: add MonthView axaml file

---
+++ Agendai/Views/Components/Agenda/MonthView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Agendai.Views.Components.Agenda;
+
+public partial class MonthView : UserControl
+{
+    public MonthView()
+    {
+        InitializeComponent();
+    }
+}
