513ee8692bee332370eb9976bc635a125785c804
Xustavo <gugaxavier2005@gmail.com>
2025-04-06 11:44:36 -0300

    refactor: add WeekView axaml file

---
+++ Agendai/Views/Components/Agenda/WeekView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Agendai.Views.Components.Agenda;
+
+public partial class WeekView : UserControl
+{
+    public WeekView()
+    {
+        InitializeComponent();
+    }
+}
