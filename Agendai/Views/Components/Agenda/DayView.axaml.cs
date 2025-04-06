61640c746537bc6764314e0aa0901b56cbaec3c4
Xustavo <gugaxavier2005@gmail.com>
2025-04-06 11:46:13 -0300

    refactor: add DayView axaml file

---
+++ Agendai/Views/Components/Agenda/DayView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Agendai.Views.Components.Agenda;
+
+public partial class DayView : UserControl
+{
+    public DayView()
+    {
+        InitializeComponent();
+    }
+}
