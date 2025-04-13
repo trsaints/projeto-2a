2c9479a1409d90753c276790086fd9b789bb8f40
trsaints <thiago.dev2k3@gmail.com>
2025-04-13 12:30:51 -0300

    update: extract new component to render list items

--- Agendai/ViewModels/TodoListViewModel.cs
+++ Agendai/ViewModels/TodoListViewModel.cs
@@ -1,15 +1,17 @@
-using System;
 using System.Collections.ObjectModel;
-using System.Linq;
 using Agendai.Models;
-using System.ComponentModel;
-using System.Windows.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
 
 
 namespace Agendai.ViewModels;
 
-public class TodoListViewModel
+public class TodoListViewModel : ObservableObject
 {
+	private ObservableCollection<Todo>? _items;
+
+	public ObservableCollection<Todo> Items
+	{
+		get => _items ?? [];
+		set => SetProperty(ref _items, value);
+	}
 }
--- Agendai/Views/Components/TodoList/TodoList.axaml.cs
+++ Agendai/Views/Components/TodoList/TodoList.axaml.cs
@@ -1,16 +1,28 @@
-using Agendai.ViewModels;
+using System.Collections.ObjectModel;
 using Avalonia.Controls;
+using Agendai.Models;
+using Avalonia;
 
 
 namespace Agendai.Views.Components.TodoList;
 
-
 public partial class TodoList : UserControl
 {
+	public static readonly StyledProperty<ObservableCollection<Todo>>
+			ItemsProperty =
+					AvaloniaProperty
+							.Register<TodoList, ObservableCollection<Todo>>(
+								nameof(Items)
+							);
+
+	public ObservableCollection<Todo> Items
+	{
+		get => GetValue(ItemsProperty);
+		set => SetValue(ItemsProperty, value);
+	}
+
 	public TodoList()
 	{
 		InitializeComponent();
-		DataContext = new TodoListViewModel();
 	}
 }
-
