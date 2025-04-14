c2db646379293fa46c3f8d18c903f0b1ee63b627
trsaints <thiago.dev2k3@gmail.com>
2025-04-14 08:19:53 -0300

    refactor: rename custom control

---
+++ Agendai/Views/Components/HomeSidebar/HomeSidebar.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+
+namespace Agendai.Views.Components.HomeSidebar;
+
+public partial class HomeSidebar : UserControl
+{
+	public HomeSidebar()
+	{
+		InitializeComponent();
+	}
+}
