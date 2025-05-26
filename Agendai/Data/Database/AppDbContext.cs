535245d67bab598ea30490d9f1302da63cc8e7c9
trsaints <thiago.dev2k3@gmail.com>
2025-05-25 22:58:08 -0300

    feat: configure database integration

---
+++ Agendai/Data/Database/AppDbContext.cs
@@ -0,0 +1,75 @@
+﻿using Agendai.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.IO;
+
+
+namespace Agendai.Data.Database;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Event> Events { get; set; }
+    public DbSet<Todo> Todos { get; set; }
+    public DbSet<Shift> Shifts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite($"Data Source={Path.Combine(DatabasePath, "agendai.db")}");
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Event>().ToTable("Events");
+        builder.Entity<Todo>().ToTable("Todos");
+        builder.Entity<Shift>().ToTable("Shifts");
+
+        builder.Entity<Event>()
+               .HasMany(e => e.Todos)
+               .WithOne(t => t.Event)
+               .HasForeignKey(t => t.EventId)
+               .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Entity<Todo>()
+                .HasMany(t => t.Shifts)
+                .WithOne(s => s.Todo)
+                .HasForeignKey(s => s.TodoId)
+                .OnDelete(DeleteBehavior.Cascade);
+    }
+
+    private string DatabasePath
+    {
+        get
+        {
+            var documents
+                = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            if (documents != string.Empty) return documents;
+
+            var desktop
+                = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            if (desktop != string.Empty) return desktop;
+
+            var userProfile
+                = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+
+            if (userProfile != string.Empty) return userProfile;
+
+            var myComputer
+                = Environment.GetFolderPath(Environment.SpecialFolder.MyComputer);
+
+            return myComputer != string.Empty
+                ? myComputer
+                : Environment.GetFolderPath(Environment.SpecialFolder.System);
+        }
+    }
+}
