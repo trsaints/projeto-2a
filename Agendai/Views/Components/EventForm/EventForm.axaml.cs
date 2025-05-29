82770496e00547fdbe16a2b9efc5eeed1b9e37e8
Xustavo <gugaxavier2005@gmail.com>
2025-05-29 17:44:19 -0300

    raw: start data apresentation in agenda window

---
+++ Agendai/Views/Components/EventForm/EventForm.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Agendai.Views.Components.EventForm;
+
+public partial class EventForm : UserControl
+{
+    public EventForm()
+    {
+        InitializeComponent();
+    }
+}
