4052dcd7ebdf36bda854653e7940c2a6fccb07f7
Xustavo <gugaxavier2005@gmail.com>
2025-06-01 11:31:13 -0300

    feat: add todo form in home sidebar

--- Agendai/ViewModels/HomeWindowViewModel.cs
+++ Agendai/ViewModels/HomeWindowViewModel.cs
@@ -8,24 +8,47 @@ public class HomeWindowViewModel : ViewModelBase
 {
 	private bool _isPopupOpen;
 
+	public TodoWindowViewModel TodoWindowVM { get; set; }
+
+	private ICommand _openPopupCommand;
+	private ICommand _openTodoFormCommand;
+	private ICommand _openAgendaCommand;
+	private ICommand _openTodoCommand;
+	private ICommand _openPomodoroCommand;
+
 	public bool IsPopupOpen
 	{
 		get => _isPopupOpen;
 		set => SetProperty(ref _isPopupOpen, value);
 	}
 
-	public ICommand OpenPopupCommand =>
-			new RelayCommand(() => IsPopupOpen = true);
+	public ICommand OpenPopupCommand => _openPopupCommand;
+	public ICommand OpenTodoFormCommand => _openTodoFormCommand;
+	public ICommand OpenAgendaCommand => _openAgendaCommand;
+	public ICommand OpenTodoCommand => _openTodoCommand;
+	public ICommand OpenPomodoroCommand => _openPomodoroCommand;
 
-	public ICommand OpenAgendaCommand => new RelayCommand(OpenAgenda);
+	public HomeWindowViewModel()
+	{
+		_openPopupCommand = new RelayCommand(() => IsPopupOpen = true);
+		_openTodoFormCommand = new RelayCommand(OpenTodoForm);
+		_openAgendaCommand = new RelayCommand(OpenAgenda);
+		_openTodoCommand = new RelayCommand(OpenTodo);
+		_openPomodoroCommand = new RelayCommand(OpenPomodoro);
+		TodoWindowVM = new TodoWindowViewModel();
+	}
 
-	public ICommand OpenTodoCommand => new RelayCommand(OpenTodo);
-
-	public ICommand OpenPomodoroCommand => new RelayCommand(OpenPomodoro);
-	
 	private void OpenAgenda() { MainViewModel?.NavigateToAgenda(); }
 
 	private void OpenTodo() { MainViewModel?.NavigateToTodo(); }
 
 	private void OpenPomodoro() { MainViewModel?.NavigateToPomodoro(); }
+
+	private void OpenTodoForm()
+	{
+		if (TodoWindowVM != null)
+		{
+			TodoWindowVM.OpenAddTask = true;
+		}
+	}
 }
