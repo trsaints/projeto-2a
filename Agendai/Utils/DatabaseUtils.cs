cca18368af64a4e11a75ee1e01e7e5f956d47d3e
trsaints <thiago.dev2k3@gmail.com>
2025-06-07 20:27:48 -0300

    feat: create utility helper for mapping database entries to observable collections for UI tracking

---
+++ Agendai/Utils/DatabaseUtils.cs
@@ -0,0 +1,20 @@
+﻿using Agendai.Data.Models;
+using Agendai.Data.Repositories.Interfaces;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace Agendai.Utils;
+
+public static class DatabaseUtils
+{
+    public static async Task MapToObservable<T>(
+        IRepository<T> repository,
+        ObservableCollection<T> targetCollection) where T : Entity
+    {
+        var items = await repository.ReadAll();
+
+        targetCollection.Clear();
+
+        foreach (var item in items ?? []) targetCollection.Add(item);
+    }
+}
