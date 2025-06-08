20ec7338ee07cc67d67422777882f18fb80c2332
trsaints <thiago.dev2k3@gmail.com>
2025-06-08 11:07:52 -0300

    feat: display recent tasks on Home window

--- Agendai/ViewModels/HomeWindowViewModel.cs
+++ Agendai/ViewModels/HomeWindowViewModel.cs
@@ -1,5 +1,9 @@
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows.Input;
+using Agendai.Data.Models;
 using Agendai.Data.Repositories.Interfaces;
+using Agendai.Utils;
 using CommunityToolkit.Mvvm.Input;
 
 
@@ -7,33 +11,39 @@ namespace Agendai.ViewModels;
 
 public class HomeWindowViewModel : ViewModelBase
 {
-	private readonly ITodoRepository _todoRepository;
+    private readonly ITodoRepository _todoRepository;
+    private readonly ObservableCollection<Todo> _todos = [];
 
-	public HomeWindowViewModel(ITodoRepository todoRepository)
-	{
+    public HomeWindowViewModel(ITodoRepository todoRepository)
+    {
         _todoRepository = todoRepository;
+        DatabaseUtils.MapToObservable(_todoRepository, _todos).Wait();
     }
-	
+
+    public ObservableCollection<Todo> RecentTodos => [
+        .. _todos.Where(t => t.Status == TodoStatus.Incomplete).Take(10)
+    ];
+
     private bool _isPopupOpen;
 
-	public bool IsPopupOpen
-	{
-		get => _isPopupOpen;
-		set => SetProperty(ref _isPopupOpen, value);
-	}
+    public bool IsPopupOpen
+    {
+        get => _isPopupOpen;
+        set => SetProperty(ref _isPopupOpen, value);
+    }
 
-	public ICommand OpenPopupCommand =>
-			new RelayCommand(() => IsPopupOpen = true);
+    public ICommand OpenPopupCommand =>
+            new RelayCommand(() => IsPopupOpen = true);
 
-	public ICommand OpenAgendaCommand => new RelayCommand(OpenAgenda);
+    public ICommand OpenAgendaCommand => new RelayCommand(OpenAgenda);
 
-	public ICommand OpenTodoCommand => new RelayCommand(OpenTodo);
+    public ICommand OpenTodoCommand => new RelayCommand(OpenTodo);
 
-	public ICommand OpenPomodoroCommand => new RelayCommand(OpenPomodoro);
-	
-	private void OpenAgenda() { MainViewModel?.NavigateToAgenda(); }
+    public ICommand OpenPomodoroCommand => new RelayCommand(OpenPomodoro);
 
-	private void OpenTodo() { MainViewModel?.NavigateToTodo(); }
+    private void OpenAgenda() { MainViewModel?.NavigateToAgenda(); }
 
-	private void OpenPomodoro() { MainViewModel?.NavigateToPomodoro(); }
+    private void OpenTodo() { MainViewModel?.NavigateToTodo(); }
+
+    private void OpenPomodoro() { MainViewModel?.NavigateToPomodoro(); }
 }
