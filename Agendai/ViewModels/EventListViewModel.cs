9b78059816d4057b76deecbfadd52f8a36cb16b1
Xustavo <gugaxavier2005@gmail.com>
2025-06-11 23:45:49 -0300

    organize view model for better readability

--- Agendai/ViewModels/EventListViewModel.cs
+++ Agendai/ViewModels/EventListViewModel.cs
@@ -13,82 +13,42 @@ namespace Agendai.ViewModels
 {
     public class EventListViewModel : ViewModelBase
     {
-        public Action? OnEventAddedOrUpdated { get; set; }
+        #region Fields
 
         private Event? _currentEvent;
-
+        private Event? _selectedEvent;
         private bool _canSave;
-        public bool CanSave
-        {
-            get => _canSave;
-            private set => SetProperty(ref _canSave, value);
-        }
+        private bool _isAddTodoPopupOpen;
+        private bool _openAddEvent;
+        private string _newEventName = string.Empty;
+        private DateTime _newDue = DateTime.Today;
+        private string _newDescription = string.Empty;
+        private string _agendaName = string.Empty;
+        private Repeats _repeat = Repeats.None;
+        private ObservableCollection<Todo> _todosForSelectedEvent = new();
+        private ObservableCollection<string> _agendaNames;
 
-        public TodoWindowViewModel TodoWindowVm { get; }
-        
+        #endregion
+
+        #region Constructor
 
         public EventListViewModel(TodoWindowViewModel todoWindowVm = null)
         {
+            TodoWindowVm = todoWindowVm;
+
             OpenPopupCommand = new RelayCommand(() => IsAddTodoPopupOpen = true);
             ClosePopupCommand = new RelayCommand(() => IsAddTodoPopupOpen = false);
             SelectTarefaCommand = new RelayCommand(() => OpenAddEvent = true);
             AddEventCommand = new RelayCommand(AddOrUpdateEvent, () => CanSave);
-            OnEventAddedOrUpdated      = () => { OpenAddEvent = false; };
-            CancelCommand = new RelayCommand(() =>
+            CancelCommand = new RelayCommand(CancelAction);
+
+            OnEventAddedOrUpdated = () => { OpenAddEvent = false; };
+
+            Events = new ObservableCollection<Event>(GenerateSampleEvents());
+            _agendaNames = new ObservableCollection<string>(Events.Select(e => e.AgendaName).Distinct());
+
+            if (TodoWindowVm != null)
             {
-                OpenAddEvent = false;
-                ClearEventForm();
-                TodoWindowVm.ClearTodoForm();
-                UpdateCanSave();
-            });
-
-
-            Events = new ObservableCollection<Event>
-            {
-                new Event(1, "Conferência da Pamonha")
-                {
-                    Description = "Conferência de pamonhas",
-                    Due = new DateTime(2025, 4, 6, 8, 0, 0),
-                    Repeats = Repeats.Anually,
-                    AgendaName = "Conferências"
-                },
-                new Event(2, "Feira da Foda")
-                {
-                    Description = "Conferência do Agro",
-                    Due = new DateTime(2025, 4, 5, 14, 0, 0),
-                    Repeats = Repeats.None,
-                    AgendaName = "Conferências"
-                },
-                new Event(3, "Festa do Peão")
-                {
-                    Description = "Festa do Peão de Barretos",
-                    Due = new DateTime(2025, 8, 20, 12, 0, 0),
-                    Repeats = Repeats.Monthly,
-                    AgendaName = "Festas"
-                },
-                new Event(4, "Feriado")
-                {
-                    Description = "Feriado de alguma coisa",
-                    Due = new DateTime(2025, 4, 18, 22, 0, 0),
-                    Repeats = Repeats.Monthly,
-                    AgendaName = "Feriados"
-                }
-            };
-
-            NewDue = DateTime.Today;
-            NewEventName = "";
-            NewDescription = "";
-            Repeat = Repeats.None;
-
-            _agendaNames = new ObservableCollection<string>(
-                Events
-                    .Select(e => e.AgendaName)
-                    .OfType<string>()
-                    .Distinct());
-
-            if (todoWindowVm != null)
-            {
-                TodoWindowVm = todoWindowVm;
                 TodoWindowVm.PropertyChanged += (s, e) =>
                 {
                     if (e.PropertyName == nameof(TodoWindowVm.NewTaskName) ||
@@ -100,17 +60,16 @@ namespace Agendai.ViewModels
             }
         }
 
+        #endregion
+
+        #region Properties
+
         public ObservableCollection<Event> Events { get; set; }
-        public ObservableCollection<Repeats> RepeatOptions { get; } = new ObservableCollection<Repeats>
+
+        public ObservableCollection<Repeats> RepeatOptions { get; } = new()
         {
-            Repeats.None,
-            Repeats.Daily,
-            Repeats.Weekly,
-            Repeats.Monthly,
-            Repeats.Anually
+            Repeats.None, Repeats.Daily, Repeats.Weekly, Repeats.Monthly, Repeats.Anually
         };
-        
-        private ObservableCollection<string> _agendaNames;
 
         public ObservableCollection<string> AgendaNames
         {
@@ -118,99 +77,60 @@ namespace Agendai.ViewModels
             set => SetProperty(ref _agendaNames, value);
         }
 
-        private bool _openAddEvent;
-        public bool OpenAddEvent
-        {
-            get => _openAddEvent;
-            set => SetProperty(ref _openAddEvent, value);
-        }
-
         public ICommand AddEventCommand { get; }
         public ICommand CancelCommand { get; }
         public ICommand SelectTarefaCommand { get; }
-        
         public ICommand OpenPopupCommand { get; }
-        
         public ICommand ClosePopupCommand { get; }
-        
-        private bool _isAddTodoPopupOpen;
+
         public bool IsAddTodoPopupOpen
         {
             get => _isAddTodoPopupOpen;
             set => SetProperty(ref _isAddTodoPopupOpen, value);
         }
 
-        private string _newEventName;
+        public bool OpenAddEvent
+        {
+            get => _openAddEvent;
+            set => SetProperty(ref _openAddEvent, value);
+        }
+
+        public bool CanSave
+        {
+            get => _canSave;
+            private set => SetProperty(ref _canSave, value);
+        }
+
         public string NewEventName
         {
             get => _newEventName;
-            set
-            {
-                if (SetProperty(ref _newEventName, value))
-                    UpdateCanSave();
-            }
+            set { if (SetProperty(ref _newEventName, value)) UpdateCanSave(); }
         }
 
-        private DateTime _newDue;
         public DateTime NewDue
         {
             get => _newDue;
-            set
-            {
-                if (SetProperty(ref _newDue, value))
-                    UpdateCanSave();
-            }
+            set { if (SetProperty(ref _newDue, value)) UpdateCanSave(); }
         }
 
-        private string _newDescription;
         public string NewDescription
         {
             get => _newDescription;
-            set
-            {
-                if (SetProperty(ref _newDescription, value))
-                    UpdateCanSave();
-            }
+            set { if (SetProperty(ref _newDescription, value)) UpdateCanSave(); }
         }
-
-        private string _agendaName;
 
         public string AgendaName
         {
             get => _agendaName;
-            set
-            {
-                if (SetProperty(ref _agendaName, value))
-                    UpdateCanSave();
-            }
+            set { if (SetProperty(ref _agendaName, value)) UpdateCanSave(); }
         }
 
-        private Repeats _repeat = Repeats.None;
         public Repeats Repeat
         {
             get => _repeat;
-            set
-            {
-                if (SetProperty(ref _repeat, value))
-                    UpdateCanSave();
-            }
+            set { if (SetProperty(ref _repeat, value)) UpdateCanSave(); }
         }
 
-        public IEnumerable<EventsByAgenda> EventsByAgendaName
-        {
-            get
-            {
-                return AgendaNames.Select(
-                    name => new EventsByAgenda
-                    {
-                        AgendaName = name,
-                        Events = new ObservableCollection<Event>(
-                            Events.Where(e => e.AgendaName == name))
-                    });
-            }
-        }
-        
-        private Event? _selectedEvent;
         public Event? SelectedEvent
         {
             get => _selectedEvent;
@@ -219,69 +139,65 @@ namespace Agendai.ViewModels
                 if (_selectedEvent != value)
                 {
                     _selectedEvent = value;
-                    OnPropertyChanged(nameof(SelectedEvent));
+                    OnPropertyChanged();
                     OnPropertyChanged(nameof(HasRelatedTodos));
                     UpdateTodosForSelectedEvent();
                 }
             }
         }
-        
-        private ObservableCollection<Todo> _todosForSelectedEvent = new();
+
         public ObservableCollection<Todo> TodosForSelectedEvent
         {
             get => _todosForSelectedEvent;
-            set
-            {
-                _todosForSelectedEvent = value;
-                OnPropertyChanged(nameof(TodosForSelectedEvent));
-            }
+            set => SetProperty(ref _todosForSelectedEvent, value);
         }
 
         public bool HasRelatedTodos => SelectedEvent?.Todos?.Any() == true;
 
-        private void UpdateTodosForSelectedEvent()
-        {
-            if (SelectedEvent != null)
-                TodosForSelectedEvent = new ObservableCollection<Todo>(SelectedEvent.Todos ?? Enumerable.Empty<Todo>());
-            else
-                TodosForSelectedEvent = new ObservableCollection<Todo>();
-        }
+        public IEnumerable<EventsByAgenda> EventsByAgendaName =>
+            AgendaNames.Select(name => new EventsByAgenda
+            {
+                AgendaName = name,
+                Events = new ObservableCollection<Event>(Events.Where(e => e.AgendaName == name))
+            });
+
+        public TodoWindowViewModel TodoWindowVm { get; }
+
+        public Action? OnEventAddedOrUpdated { get; set; }
+
+        #endregion
+
+        #region Public Methods
 
         public void LoadEvent(Event? ev)
         {
             _currentEvent = ev;
-
             NewEventName = ev?.Name ?? "";
             NewDescription = ev?.Description ?? "";
             NewDue = ev?.Due ?? DateTime.Today;
             Repeat = ev?.Repeats ?? Repeats.None;
             AgendaName = ev?.AgendaName ?? "";
             SelectedEvent = _currentEvent;
-
             UpdateCanSave();
         }
 
         public void UpdateCanSave()
         {
             if (TodoWindowVm == null) return;
-            
+
             bool hasTaskChanges = 
                 !string.IsNullOrWhiteSpace(TodoWindowVm.NewTaskName?.Trim()) ||
                 !string.IsNullOrWhiteSpace(TodoWindowVm.SelectedTodoName?.Trim());
 
-            CanSave =
-                !string.IsNullOrWhiteSpace(NewEventName) &&
-                (
-                    _currentEvent == null ||
-                    NewEventName != _currentEvent.Name ||
-                    NewDescription != _currentEvent.Description ||
-                    NewDue.Date != _currentEvent.Due.Date || 
-                    Repeat != _currentEvent.Repeats ||
-                    AgendaName != _currentEvent.AgendaName ||
-                    hasTaskChanges
-                );
-
-
+            CanSave = !string.IsNullOrWhiteSpace(NewEventName) && (
+                _currentEvent == null ||
+                NewEventName != _currentEvent.Name ||
+                NewDescription != _currentEvent.Description ||
+                NewDue.Date != _currentEvent.Due.Date ||
+                Repeat != _currentEvent.Repeats ||
+                AgendaName != _currentEvent.AgendaName ||
+                hasTaskChanges
+            );
 
             ((RelayCommand)AddEventCommand).NotifyCanExecuteChanged();
         }
@@ -292,7 +208,7 @@ namespace Agendai.ViewModels
 
             if (_currentEvent == null)
             {
-                var newEvent = new Event(Convert.ToUInt32(Events.Count + 1), NewEventName)
+                var newEvent = new Event((uint)(Events.Count + 1), NewEventName)
                 {
                     Description = NewDescription,
                     Due = NewDue,
@@ -301,53 +217,16 @@ namespace Agendai.ViewModels
                     Todos = new List<Todo>()
                 };
 
-                if (TodoWindowVm.SelectedTodo != null)
-                {
-                    newEvent.Todos.Add(TodoWindowVm.SelectedTodo);
-                }
-                if (!string.IsNullOrWhiteSpace(TodoWindowVm?.NewTaskName))
-                {
-                    var newTodo = TodoWindowVm.AddTodo(newEvent);
-                    if (newTodo != null)
-                    {
-                        newEvent.Todos.Add(newTodo);
-                    }
-                }
-
+                AddTodosToEvent(newEvent);
                 Events.Add(newEvent);
             }
             else
             {
-                _currentEvent.Name = NewEventName;
-                _currentEvent.Description = NewDescription;
-                _currentEvent.Due = NewDue;
-                _currentEvent.Repeats = Repeat;
-                _currentEvent.AgendaName = AgendaName;
-
-                if (_currentEvent.Todos == null)
-                    _currentEvent.Todos = new List<Todo>();
-
-                if (TodoWindowVm.SelectedTodo != null)
-                {
-                    if (!_currentEvent.Todos.Contains(TodoWindowVm.SelectedTodo))
-                    {
-                        TodoWindowVm.AddTodo(_currentEvent);
-                        _currentEvent.Todos.Add(TodoWindowVm.SelectedTodo);
-                    }
-                }
-                
-                if (!string.IsNullOrWhiteSpace(TodoWindowVm?.NewTaskName))
-                {
-                    var newTodo = TodoWindowVm.AddTodo(_currentEvent);
-                    if (newTodo != null && !_currentEvent.Todos.Contains(newTodo))
-                    {
-                        _currentEvent.Todos.Add(newTodo);
-                    }
-                }
+                UpdateEventProperties(_currentEvent);
+                AddTodosToEvent(_currentEvent);
             }
 
             ClearEventForm();
-
             OnEventAddedOrUpdated?.Invoke();
             UpdateCanSave();
         }
@@ -364,6 +243,86 @@ namespace Agendai.ViewModels
             SelectedEvent = null;
         }
 
+        #endregion
 
+        #region Private Helpers
+
+        private void CancelAction()
+        {
+            OpenAddEvent = false;
+            ClearEventForm();
+            TodoWindowVm.ClearTodoForm();
+            UpdateCanSave();
+        }
+
+        private void UpdateTodosForSelectedEvent()
+        {
+            TodosForSelectedEvent = new ObservableCollection<Todo>(
+                SelectedEvent?.Todos ?? Enumerable.Empty<Todo>());
+        }
+
+        private void AddTodosToEvent(Event ev)
+        {
+            if (TodoWindowVm.SelectedTodo != null && !ev.Todos.Contains(TodoWindowVm.SelectedTodo))
+            {
+                TodoWindowVm.AddTodo(ev);
+                ev.Todos.Add(TodoWindowVm.SelectedTodo);
+            }
+
+            if (!string.IsNullOrWhiteSpace(TodoWindowVm.NewTaskName))
+            {
+                var newTodo = TodoWindowVm.AddTodo(ev);
+                if (newTodo != null && !ev.Todos.Contains(newTodo))
+                {
+                    ev.Todos.Add(newTodo);
+                }
+            }
+        }
+
+        private void UpdateEventProperties(Event ev)
+        {
+            ev.Name = NewEventName;
+            ev.Description = NewDescription;
+            ev.Due = NewDue;
+            ev.Repeats = Repeat;
+            ev.AgendaName = AgendaName;
+
+            if (ev.Todos == null)
+                ev.Todos = new List<Todo>();
+        }
+
+        private IEnumerable<Event> GenerateSampleEvents() => new[]
+        {
+            new Event(1, "Conferência da Pamonha")
+            {
+                Description = "Conferência de pamonhas",
+                Due = new DateTime(2025, 4, 6, 8, 0, 0),
+                Repeats = Repeats.Anually,
+                AgendaName = "Conferências"
+            },
+            new Event(2, "Feira da Foda")
+            {
+                Description = "Conferência do Agro",
+                Due = new DateTime(2025, 4, 5, 14, 0, 0),
+                Repeats = Repeats.None,
+                AgendaName = "Conferências"
+            },
+            new Event(3, "Festa do Peão")
+            {
+                Description = "Festa do Peão de Barretos",
+                Due = new DateTime(2025, 8, 20, 12, 0, 0),
+                Repeats = Repeats.Monthly,
+                AgendaName = "Festas"
+            },
+            new Event(4, "Feriado")
+            {
+                Description = "Feriado de alguma coisa",
+                Due = new DateTime(2025, 4, 18, 22, 0, 0),
+                Repeats = Repeats.Monthly,
+                AgendaName = "Feriados"
+            }
+        };
+
+        #endregion
     }
 }
