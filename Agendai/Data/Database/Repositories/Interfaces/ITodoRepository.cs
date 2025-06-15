ae29259106881af0d520ac6278d39cae9b8feb68
trsaints <thiago.dev2k3@gmail.com>
2025-06-15 16:39:10 -0300

    feat: implement Repository<Todo> specialization

---
+++ Agendai/Data/Database/Repositories/Interfaces/ITodoRepository.cs
@@ -0,0 +1,7 @@
+﻿using Agendai.Data.Models;
+
+namespace Agendai.Data.Database.Repositories.Interfaces;
+
+public interface ITodoRepository : IRepository<Todo>
+{
+}
---
+++ Agendai/Data/Database/Repositories/TodoRepository.cs
@@ -0,0 +1,12 @@
+﻿using Agendai.Data.Database.Context;
+using Agendai.Data.Database.Repositories.Interfaces;
+using Agendai.Data.Models;
+
+namespace Agendai.Data.Database.Repositories;
+
+public class TodoRepository : Repository<Todo>, ITodoRepository
+{
+    public TodoRepository(AppDbContext db) : base(db)
+    {
+    }
+}
