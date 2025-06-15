fea93edb0379649e6d0c0438a1f31d79b2fc6067
trsaints <thiago.dev2k3@gmail.com>
2025-06-15 11:13:19 -0300

    feat: create the first database migration

---
+++ Agendai/Data/Database/Migrations/20250615140821_Schema.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Agendai.Data.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Schema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    Id = table.Column<ulong>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    AgendaName = table.Column<string>(type: "TEXT", nullable: true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 128, nullable: false),
+                    Repeats = table.Column<int>(type: "INTEGER", nullable: false),
+                    Reminders = table.Column<string>(type: "TEXT", nullable: true),
+                    InitialDue = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Due = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 256, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Todos",
+                columns: table => new
+                {
+                    Id = table.Column<ulong>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ListName = table.Column<string>(type: "TEXT", maxLength: 64, nullable: true),
+                    FinishedShifts = table.Column<uint>(type: "INTEGER", nullable: false),
+                    TotalShifts = table.Column<uint>(type: "INTEGER", nullable: false),
+                    EventId = table.Column<ulong>(type: "INTEGER", nullable: true),
+                    Status = table.Column<int>(type: "INTEGER", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 128, nullable: false),
+                    Repeats = table.Column<int>(type: "INTEGER", nullable: false),
+                    Reminders = table.Column<string>(type: "TEXT", nullable: true),
+                    InitialDue = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Due = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 256, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Todos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Todos_Events_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Events",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Shifts",
+                columns: table => new
+                {
+                    Id = table.Column<ulong>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Duration = table.Column<TimeOnly>(type: "TEXT", nullable: false),
+                    Status = table.Column<int>(type: "INTEGER", nullable: false),
+                    TodoId = table.Column<ulong>(type: "INTEGER", nullable: true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 128, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shifts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Shifts_Todos_TodoId",
+                        column: x => x.TodoId,
+                        principalTable: "Todos",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Shifts_TodoId",
+                table: "Shifts",
+                column: "TodoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Todos_EventId",
+                table: "Todos",
+                column: "EventId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Shifts");
+
+            migrationBuilder.DropTable(
+                name: "Todos");
+
+            migrationBuilder.DropTable(
+                name: "Events");
+        }
+    }
+}
