455b382d655b75410e77ec75090ecef4e03299b2
trsaints <thiago.dev2k3@gmail.com>
2025-06-15 16:40:44 -0300

    feat: implement Repository<Shift> specialization

---
+++ Agendai/Data/Database/Repositories/ShiftRepository.cs
@@ -0,0 +1,12 @@
+﻿using Agendai.Data.Database.Context;
+using Agendai.Data.Database.Repositories.Interfaces;
+using Agendai.Data.Models;
+
+namespace Agendai.Data.Database.Repositories;
+
+public class ShiftRepository : Repository<Shift>, IShiftRepository
+{
+    public ShiftRepository(AppDbContext db) : base(db)
+    {
+    }
+}
