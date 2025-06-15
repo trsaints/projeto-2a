4a5326009aed1aa062eb2d03cbd7fd36043102d7
trsaints <thiago.dev2k3@gmail.com>
2025-06-15 12:49:42 -0300

    feat: add base repository abstraction

---
+++ Agendai/Data/Database/Repositories/Interfaces/Repository.cs
@@ -0,0 +1,50 @@
+﻿using Agendai.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Agendai.Data.Database.Repositories.Interfaces;
+
+class Repository<T> : IRepository<T>
+    where T : Entity
+{
+    public Task<T?> AddAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual Task<T?> DeleteAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual Task<bool> ExistsAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<T>?> FindAsync(Predicate<Func<T, bool>> predicate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual Task<IEnumerable<T>?> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T?> GetByIdAsync(ulong id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T?> GetByNameAsync(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T?> UpdateAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+}
