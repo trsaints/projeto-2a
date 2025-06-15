89ddbffead2faa9725de7e4f71e3902ee0b48302
trsaints <thiago.dev2k3@gmail.com>
2025-06-15 09:58:40 -0300

    update: use default method body instead of body expression

--- Agendai/Program.cs
+++ Agendai/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Avalonia;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 
 namespace Agendai;
@@ -9,8 +10,11 @@ sealed class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        BuildAvaloniaApp()
+       .StartWithClassicDesktopLifetime(args);
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
