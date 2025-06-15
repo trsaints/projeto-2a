14beb2939e12e013a529286bed438bf6cbee96c4
trsaints <thiago.dev2k3@gmail.com>
2025-06-15 16:41:58 -0300

    feat: implement Repository<Event> specialization

---
+++ Agendai/Data/Database/Repositories/EventRepository.cs
@@ -0,0 +1,12 @@
+﻿using Agendai.Data.Database.Context;
+using Agendai.Data.Database.Repositories.Interfaces;
+using Agendai.Data.Models;
+
+namespace Agendai.Data.Database.Repositories;
+
+public class EventRepository : Repository<Event>, IEventRepository
+{
+    public EventRepository(AppDbContext db) : base(db)
+    {
+    }
+}
---
+++ Agendai/Data/Database/Repositories/Interfaces/IEventRepository.cs
@@ -0,0 +1,7 @@
+﻿using Agendai.Data.Models;
+
+namespace Agendai.Data.Database.Repositories.Interfaces;
+
+public interface IEventRepository : IRepository<Event>
+{
+}
