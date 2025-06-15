cd73dc0db31019032948d9323fae774ccc8c6872
trsaints <thiago.dev2k3@gmail.com>
2025-06-15 10:37:25 -0300

    update: configure Shift entity for database mapping

--- Agendai/Data/Models/Shift.cs
+++ Agendai/Data/Models/Shift.cs
@@ -1,11 +1,28 @@
 using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 
 namespace Agendai.Data.Models;
 
-
-public class Shift(ulong id, string name) : Entity(id, name)
+[Table("Shifts")]
+public class Shift : Entity
 {
-	public TimeOnly Duration { get; set; }
+	public Shift(ulong id, string name) : base(id, name)
+    {
+    }
+
+    public Shift() { }
+
+    [Required]
+    [MinLength(30)]
+    public TimeOnly Duration { get; set; }
+
+    [DefaultValue(ShiftStatus.Incomplete)]
 	public ShiftStatus Status { get; set; }
+
+    [ForeignKey(nameof(Todo))]
+    public ulong? TodoId { get; set; }
+    public virtual Todo? Todo { get; set; }
 }
