6bf104e11759e74c758b6caa18dd48321e48aa59
trsaints <thiago.dev2k3@gmail.com>
2025-06-15 11:12:49 -0300

    feat: add dotnet-ef design time factory

---
+++ Agendai/Data/Database/Context/DbFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agendai.Data.Database.Context;
+
+class DbFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+        var databasePath = AppDbContext.GetDatabasePath();
+        var dbFile = Path.Combine(databasePath, "agendai.db");
+
+        optionsBuilder
+            .UseSqlite($"Data Source={dbFile}",
+                options => options.MigrationsAssembly("Agendai"))
+            .UseLazyLoadingProxies();
+
+        return new AppDbContext(optionsBuilder.Options);
+    }
+}
