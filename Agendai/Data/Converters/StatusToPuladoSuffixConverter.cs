f7a9a7c3e2dbb2cbecab5dc65f6930c484626758
Charles Santos <coliveirasantos1234@gmail.com>
2025-06-17 10:39:22 -0300

    feat: add converters for task status display and strikethrough styling

---
+++ Agendai/Data/Converters/StatusToPuladoSuffixConverter.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia.Data.Converters;
+using Agendai.Data.Models;
+using System;
+using System.Globalization;
+
+namespace Agendai.Data.Converters;
+public class StatusToPuladoSuffixConverter : IValueConverter
+{
+    public static readonly StatusToPuladoSuffixConverter Instance = new();
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value is TodoStatus status && status == TodoStatus.Skipped
+            ? " (pulado)"
+            : string.Empty;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
---
+++ Agendai/Data/Converters/StatusToStrikethroughConverter.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia.Data.Converters;
+using Avalonia.Media;
+using Agendai.Data.Models;
+using System;
+using System.Globalization;
+
+namespace Agendai.Data.Converters
+{
+    public class StatusToStrikethroughConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is TodoStatus status && status == TodoStatus.Skipped)
+                return TextDecorations.Strikethrough;
+
+            return null;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
