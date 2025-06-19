4474077a5f1622a6e49838d503b9d2cda3d4cda7
Xustavo <gugaxavier2005@gmail.com>
2025-06-19 20:40:56 -0300

    add auto complete box to day view and appearence of modal when clicked

--- Agendai/Views/Components/Agenda/DayView.axaml.cs
+++ Agendai/Views/Components/Agenda/DayView.axaml.cs
@@ -1,7 +1,9 @@
-﻿using Agendai.ViewModels;
+﻿using System.Linq;
+using Agendai.ViewModels;
 using Agendai.ViewModels.Agenda;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using Avalonia.VisualTree;
@@ -31,5 +33,41 @@ public partial class DayView : UserControl
     {
         var parentAgenda = this.FindAncestorOfType<Agenda>();
         parentAgenda?.OnEventOrTodoCLicked(sender, e);
+    }
+    
+    private void SearchText_OnGotFocus(object? sender, GotFocusEventArgs e)
+    {
+        var autoComplete = (AutoCompleteBox)sender;
+        autoComplete.IsDropDownOpen = true;
+    }
+    
+    private void SearchBox_TextChanged(object? sender, RoutedEventArgs e)
+    {
+        if (sender is AutoCompleteBox box)
+        {
+            if (this.DataContext is AgendaWindowViewModel vm)
+            {
+                vm.SearchText = box.Text;
+            }
+        }
+    }
+    
+    private void SearchBox_SelectionChanged(object? sender, SelectionChangedEventArgs e)
+    {
+        if (sender is AutoCompleteBox box && box.SelectedItem is string selectedName)
+        {
+            var viewModel = DataContext as AgendaWindowViewModel;
+
+            var ev = viewModel?.EventList.Events.FirstOrDefault(x => x.Name == selectedName);
+            var todo = viewModel?.TodoList.Todos.FirstOrDefault(x => x.Name == selectedName);
+
+            if (ev != null || todo != null)
+            {
+                var fakeButton = new Button { Tag = ev ?? (object)todo };
+                var parentAgenda = this.FindAncestorOfType<Agenda>();
+
+                parentAgenda?.OnEventOrTodoCLicked(fakeButton, e);
+            }
+        }
     }
 }
