e5ad9ffbbba6997ed6dae80c2ca75a36dcbd8fdf
trsaints <thiago.dev2k3@gmail.com>
2025-06-20 01:06:58 -0300

    update: add Serilog for extensive logging implementation

--- Agendai/Program.cs
+++ Agendai/Program.cs
@@ -1,38 +1,61 @@
 ﻿using Avalonia;
+using Avalonia.Logging;
 using System;
 using System.Threading.Tasks;
+using Serilog;
 
 
 namespace Agendai;
 
 sealed class Program
 {
-    // Initialization code. Don't use any Avalonia, third-party APIs or any
-    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-    // yet and stuff might break.
-    [STAThread]
-    public static void Main(string[] args)
-    {
-	    // Captura exceções não tratadas no domínio do aplicativo
-	    AppDomain.CurrentDomain.UnhandledException += (sender, e) =>
-	    {
-		    Console.WriteLine($"Unhandled exception: {e.ExceptionObject}");
-	    };
+	// Initialization code. Don't use any Avalonia, third-party APIs or any
+	// SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+	// yet and stuff might break.
+	[STAThread]
+	public static void Main(string[] args)
+	{
+		// Configuração do Serilog
+		Log.Logger = new LoggerConfiguration()
+		             .MinimumLevel.Debug()
+		             .WriteTo.Console()
+		             .CreateLogger();
 
-	    // Captura exceções não observadas em tarefas
-	    TaskScheduler.UnobservedTaskException += (sender, e) =>
-	    {
-		    Console.WriteLine($"Unobserved task exception: {e.Exception}");
-		    e.SetObserved(); // Marca a exceção como observada
-	    };
-	    
-	    BuildAvaloniaApp()
-			    .StartWithClassicDesktopLifetime(args);
-    }
-    // Avalonia configuration, don't remove; also used by visual designer.
-    public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
-            .UsePlatformDetect()
-            .WithInterFont()
-            .LogToTrace();
+		// Captura exceções não observadas em tarefas
+		TaskScheduler.UnobservedTaskException += (sender, e) =>
+		{
+			Log.Error(e.Exception, "Unobserved task exception");
+			e.SetObserved(); // Marca a exceção como observada
+		};
+
+		// Captura exceções globais
+		AppDomain.CurrentDomain.UnhandledException += (sender, e) =>
+		{
+			Log.Fatal(e.ExceptionObject as Exception, "Unhandled exception");
+		};
+
+		try
+		{
+			BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+		}
+		catch (Exception ex)
+		{
+			Log.Fatal(ex, "Application terminated unexpectedly");
+			throw;
+		}
+		finally
+		{
+			Log.CloseAndFlush();
+		}
+	}
+
+	// Avalonia configuration, don't remove; also used by visual designer.
+	public static AppBuilder BuildAvaloniaApp()
+	{
+		Log.Logger = new LoggerConfiguration().MinimumLevel.Debug()
+		                                      .WriteTo.Console()
+		                                      .CreateLogger();
+
+		return AppBuilder.Configure<App>().UsePlatformDetect().LogToTrace();
+	}
 }
