5e51aed247b3dca7d61da5a64e9c131abe0675a1
Xustavo <gugaxavier2005@gmail.com>
2025-06-20 20:36:59 -0300

    add auto complete box to add event to list name or relate task to event

--- Agendai/Views/Components/EventList/AddEvent.axaml.cs
+++ Agendai/Views/Components/EventList/AddEvent.axaml.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Linq;
 using Agendai.Data.Models;
 using Avalonia;
 using Avalonia.Controls;
 using Agendai.ViewModels;
+using Avalonia.Input;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
@@ -13,6 +15,34 @@ public partial class AddEvent : UserControl
     public AddEvent()
     {
         InitializeComponent();
+    }
+    
+    private void SearchText_OnGotFocus(object? sender, GotFocusEventArgs e)
+    {
+        var autoComplete = (AutoCompleteBox)sender;
+        autoComplete.IsDropDownOpen = true;
+    }
+    
+    private void AddSearchBox_TextChanged(object? sender, RoutedEventArgs e)
+    {
+        if (sender is AutoCompleteBox box)
+        {
+            if (this.DataContext is EventListViewModel vm)
+            {
+                Console.WriteLine($"texto: {box.Text}");
+            }
+        }
+    }
+    
+    private void SelectTodoSearchBox_TextChanged(object? sender, RoutedEventArgs e)
+    {
+        if (sender is AutoCompleteBox box)
+        {
+            if (this.DataContext is EventListViewModel vm)
+            {
+                Console.WriteLine($"texto: {box.Text}");
+            }
+        }
     }
 
 }
