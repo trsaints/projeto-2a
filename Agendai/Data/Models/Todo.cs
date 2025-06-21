1ee79a21b6ea591e9b24b860359b829155c2e256
trsaints <thiago.dev2k3@gmail.com>
2025-06-20 21:40:44 -0300

    feat: create static utility to generate sample Todo items

--- Agendai/Data/Models/Todo.cs
+++ Agendai/Data/Models/Todo.cs
@@ -1,20 +1,23 @@
 using System.ComponentModel;
 using System;
+using System.Collections.Generic;
+
 
 namespace Agendai.Data.Models;
 
-
-public class Todo(ulong id, string name) : Recurrence(id, name), INotifyPropertyChanged
+public class Todo(ulong id, string name)
+		: Recurrence(id, name), INotifyPropertyChanged
 {
-	public string    ListName       { get; set; }
-	public uint       FinishedShifts { get; set; }
-	public uint       TotalShifts    { get; set; }
-	public virtual Event? RelatedEvent { get; set; }
-	
+	public         string? ListName       { get; set; }
+	public         uint    FinishedShifts { get; set; }
+	public         uint    TotalShifts    { get; set; }
+	public virtual Event?  RelatedEvent   { get; set; }
+
 	private TodoStatus _status;
 	public TodoStatus Status
 	{
 		get => _status;
+
 		set
 		{
 			if (_status != value)
@@ -26,13 +29,74 @@ public class Todo(ulong id, string name) : Recurrence(id, name), INotifyProperty
 		}
 	}
 
-	public string? SkippedDisplay => Status == TodoStatus.Skipped ? " (pulado)" : string.Empty;
+	public string? SkippedDisplay =>
+			Status == TodoStatus.Skipped ? " (pulado)" : string.Empty;
 
-    public event PropertyChangedEventHandler PropertyChanged;
-	public event Action<Todo, TodoStatus>? OnStatusChanged;
+	public new event PropertyChangedEventHandler? PropertyChanged;
+	public event     Action<Todo, TodoStatus>?    OnStatusChanged;
 
-	protected virtual void OnPropertyChanged(string propertyName)
+	protected new virtual void OnPropertyChanged(string propertyName)
 	{
-		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		PropertyChanged?.Invoke(
+			this,
+			new PropertyChangedEventArgs(propertyName)
+		);
+	}
+
+	public static List<Todo> Samples()
+	{
+		return
+		[
+			new Todo(1, "Comprar p�o")
+			{
+				ListName    = "Casa", Status    = TodoStatus.Incomplete,
+				TotalShifts = 1, FinishedShifts = 0
+			},
+			new Todo(2, "Lavar a lou�a")
+			{
+				ListName    = "Casa", Status    = TodoStatus.Incomplete,
+				TotalShifts = 1, FinishedShifts = 0
+			},
+			new Todo(3, "Estudar matem�tica")
+			{
+				ListName    = "Estudos", Status = TodoStatus.Incomplete,
+				TotalShifts = 2, FinishedShifts = 0
+			},
+			new Todo(4, "Ler cap�tulo de livro")
+			{
+				ListName    = "Estudos", Status = TodoStatus.Incomplete,
+				TotalShifts = 1, FinishedShifts = 0
+			},
+			new Todo(5, "Enviar relat�rio")
+			{
+				ListName    = "Trabalho", Status = TodoStatus.Incomplete,
+				TotalShifts = 1, FinishedShifts  = 0
+			},
+			new Todo(6, "Reuni�o com equipe")
+			{
+				ListName    = "Trabalho", Status = TodoStatus.Incomplete,
+				TotalShifts = 1, FinishedShifts  = 0
+			},
+			new Todo(7, "Passear com o cachorro")
+			{
+				ListName    = "Casa", Status    = TodoStatus.Incomplete,
+				TotalShifts = 1, FinishedShifts = 0
+			},
+			new Todo(8, "Revisar anota��es")
+			{
+				ListName    = "Estudos", Status = TodoStatus.Incomplete,
+				TotalShifts = 1, FinishedShifts = 0
+			},
+			new Todo(9, "Organizar documentos")
+			{
+				ListName    = "Trabalho", Status = TodoStatus.Incomplete,
+				TotalShifts = 1, FinishedShifts  = 0
+			},
+			new Todo(10, "Limpar o quarto")
+			{
+				ListName    = "Casa", Status    = TodoStatus.Incomplete,
+				TotalShifts = 1, FinishedShifts = 0
+			}
+		];
 	}
 }
