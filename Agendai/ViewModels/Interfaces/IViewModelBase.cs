35f6cd15a503a6b4e972493971722ed3a307669f
trsaints <thiago.dev2k3@gmail.com>
2025-06-22 16:18:31 -0300

    feat: implement interface for ViewModelBase

---
+++ Agendai/ViewModels/Interfaces/IViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Input;
+
+
+namespace Agendai.ViewModels.Interfaces;
+
+public interface IViewModelBase
+{
+	public  MainWindowViewModel? MainViewModel     { get; set; }
+	public  ICommand             ReturnHomeCommand { get; }
+	private void                 ReturnHome()      { MainViewModel?.NavigateToHome(); }
+}
